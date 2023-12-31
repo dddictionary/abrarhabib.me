20568c55fccd72707201d86e5dbadb74b89ff740
Abrar Habib <abrarhabib285@gmail.com>
2023-12-30 21:36:24 -0500

    set up react rotuer on portfolio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="logo">
+        <h1>Logo Here</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
